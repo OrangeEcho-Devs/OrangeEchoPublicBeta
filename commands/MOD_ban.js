3bd12db126a00a68362b0ccd2a7eda8a2dbd785f
Orange Group <orangemanagementcorpn@gmail.com>
2020-05-23 17:44:46 +0800

    23rd edit

--- commands/MOD_ban.js
+++ commands/MOD_ban.js
@@ -7,7 +7,7 @@ module.exports = {
 	mod:true,
     execute(message, args, client) {
         try {
-			if (message.author.id == message.mentions.members.first().id){respond('',`You can't perform this action on yourself.`, message.channel);return;}
+			//if (message.author.id == message.mentions.members.first().id){respond('',`You can't perform this action on yourself.`, message.channel);return;}
 			const {ModeratorRoleID} = require('../config.json');
 			const checkmemberforroles = message.mentions.members.first()
 			if (checkmemberforroles.roles.cache.some(role => role.id === `${ModeratorRoleID}`)){respond('',`You can't perform that action on this user.`, message.channel);return;;return;}
